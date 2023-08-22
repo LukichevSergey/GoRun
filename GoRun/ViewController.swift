d681db4027eec2e488b9cb76e609446c0f9ace06
Lukichev Sergey <Lukichev.sk@dns-shop.ru>
2023-08-22 19:08:45 +0700

    Initial Commit

---
+++ GoRun/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GoRun
+//
+//  Created by Лукичев Сергей on 22.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
